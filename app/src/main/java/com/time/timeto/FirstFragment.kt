11ec93041be38ba28a960f42f164cffe6f74eec8
Tomasz Zaniewski <tomasz.zaniewski@gmail.com>
2023-07-18 23:04:22 +0200

    data class and set title

--- app/src/main/java/com/time/timeto/FirstFragment.kt
+++ app/src/main/java/com/time/timeto/FirstFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.time.timeto.databinding.FragmentFirstBinding
+import java.time.LocalDateTime
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -31,10 +33,15 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val eventDate = LocalDateTime.of(2023,8,9,13,30)
+        val createdDate = LocalDateTime.of(2023,7,18,22,19)
+        val plnTrip = TimeToEvent("Poland Trip", "HH", eventDate, createdDate)
+        val heading = plnTrip.timeUntil()
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        val titleTextView = view.findViewById<TextView>(R.id.textview_title)
+        titleTextView.text = plnTrip.name
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/time/timeto/TimeToEvent.kt
@@ -0,0 +1,16 @@
+package com.time.timeto
+
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+data class TimeToEvent(
+    val name: String,
+    val format: String,
+    val eventDate: LocalDateTime,
+    val createdDate: LocalDateTime
+) {
+
+    fun timeUntil(): Long {
+        return ChronoUnit.HOURS.between(LocalDateTime.now(), eventDate)
+    }
+}
