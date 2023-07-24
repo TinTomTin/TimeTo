17fde8c8f769f4ac5a3c4b1b4bbb387e7ccb11db
Tomasz Zaniewski <tomasz.zaniewski@gmail.com>
2023-07-24 23:12:45 +0200

    finished serialization

--- app/src/main/java/com/time/timeto/TimeToEvent.kt
+++ app/src/main/java/com/time/timeto/TimeToEvent.kt
@@ -1,21 +1,36 @@
 package com.time.timeto
 
 import kotlinx.serialization.Contextual
+import kotlinx.serialization.KSerializer
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 data class TimeToEvent(
     val name: String,
     val format: String,
-    @Contextual
+    @Serializable(DateTimeSerializer::class)
     val eventDate: LocalDateTime,
-    @Contextual
+    @Serializable(DateTimeSerializer::class)
     val createdDate: LocalDateTime
 ) {
 
     fun timeUntil(): Long {
         return ChronoUnit.HOURS.between(LocalDateTime.now(), eventDate)
+    }
+}
+
+object DateTimeSerializer : KSerializer<LocalDateTime> {
+    override val descriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
+    //TODO: specify datetime format explicitly
+    override fun serialize(encoder: Encoder, dtValue: LocalDateTime) = encoder.encodeString(dtValue.toString())
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        val input = decoder.decodeString()
+        return LocalDateTime.parse(input)
     }
 }
--- app/src/test/java/com/time/timeto/ExampleUnitTest.kt
+++ app/src/test/java/com/time/timeto/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.time.timeto
 
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.json.Json
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.time.LocalDateTime
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,35 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun serialize_event(){
+        val eventDate = LocalDateTime.of(2023,6,8,13,0)
+        val createdDate = LocalDateTime.of(2023,5,18,22,19)
+        val testEvent = TimeToEvent("Test Event", "HH", eventDate, createdDate)
+        val serialized = Json.encodeToString(TimeToEvent.serializer(), testEvent)
+        assertEquals("{\"name\":\"Test Event\",\"format\":\"HH\",\"eventDate\":\"2023-06-08T13:00\",\"createdDate\":\"2023-05-18T22:19\"}", serialized)
+    }
+
+    @Test
+    fun serialize_event_list(){
+        val eventDate = LocalDateTime.of(2023,6,8,13,0)
+        val createdDate = LocalDateTime.of(2023,5,18,22,19)
+        val testEventOne = TimeToEvent("One", "HH", eventDate, createdDate)
+        val testEventTwo = TimeToEvent("Two", "DD", eventDate, createdDate)
+        val events : List<TimeToEvent> = listOf(testEventOne, testEventTwo)
+        val lstSerializer = ListSerializer(TimeToEvent.serializer())
+        val expected = "[{\"name\":\"One\",\"format\":\"HH\",\"eventDate\":\"2023-06-08T13:00\",\"createdDate\":\"2023-05-18T22:19\"},{\"name\":\"Two\",\"format\":\"DD\",\"eventDate\":\"2023-06-08T13:00\",\"createdDate\":\"2023-05-18T22:19\"}]"
+        assertEquals(expected, Json.encodeToString(lstSerializer, events))
+    }
+
+    @Test
+    fun deserialize_event(){
+        val eventString = "{\"name\":\"Test Event\",\"format\":\"HH\",\"eventDate\":\"2023-06-08T13:00\",\"createdDate\":\"2023-05-18T22:19\"}"
+        val event = Json.decodeFromString(TimeToEvent.serializer(),eventString)
+        val eventDate = LocalDateTime.of(2023,6,8,13,0)
+        val createdDate = LocalDateTime.of(2023,5,18,22,19)
+        val expectedEvent = TimeToEvent("Test Event", "HH", eventDate, createdDate)
+        assertEquals(expectedEvent, event)
     }
 }
